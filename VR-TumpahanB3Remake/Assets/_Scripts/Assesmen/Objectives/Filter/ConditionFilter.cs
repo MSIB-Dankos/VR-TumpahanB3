f39695e04776f99af0748835ae0ae451f2a1c882
Azhar564 <azharramadhan7777@gmail.com>
2024-11-19 15:45:31 +0700

    Update Game

---
+++ VR-TumpahanB3Remake/Assets/_Scripts/Assesmen/Objectives/Filter/ConditionFilter.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Filtering;
+
+public class ConditionFilter : MonoBehaviour, IXRSelectFilter
+{
+    [field: SerializeField] public bool allowed { get; set; }
+
+    bool IXRSelectFilter.canProcess => allowed;
+
+    bool IXRSelectFilter.Process(IXRSelectInteractor interactor, IXRSelectInteractable interactable)
+    {
+        return allowed;
+    }
+}
